c84e6cc99fa4c7530520578f0035f66b6bf4660b
aneeshjain <aneeshjain70@gmail.com>
2023-06-01 19:37:39 -0700

    Added App.js

---
+++ src/test_data.js
@@ -0,0 +1,75 @@
+const dummyBusinesses = [
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flavortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian',
+        rating: 4.5,
+        reviewCount: 90
+    },
+
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'Osha Thai Noodle Cafe',
+        address: '1234 Noodle Street',
+        city: 'San Francisco',
+        state: 'CA',
+        zipCode: '12345',
+        category: 'Asian',
+        rating: 4.0,
+        reviewCount: 123
+    },
+    
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'Sun and Moon Cafe',
+        address: '452 Moon Street',
+        city: 'Miami',
+        state: 'FL',
+        zipCode: '10101',
+        category: 'Thai',
+        rating: 3.5,
+        reviewCount: 50
+    },
+
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'Dominos',
+        address: '125 Pizza Street',
+        city: 'Pizza Town',
+        state: 'NY',
+        zipCode: '10431',
+        category: 'Italian',
+        rating: 5.0,
+        reviewCount: 900
+    },
+
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'Mehfil Indian Cuisine',
+        address: '321 Curry Street',
+        city: 'Seattle',
+        state: 'WA',
+        zipCode: '45321',
+        category: 'Indian',
+        rating: 4.5,
+        reviewCount: 90
+    },
+
+    {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'Panda Express',
+        address: '1234 Orange Chicken Street',
+        city: 'Los Angeles',
+        state: 'CA',
+        zipCode: '90210',
+        category: 'Chinese',
+        rating: 4.5,
+        reviewCount: 9000
+    },
+];
+
+export default dummyBusinesses;
