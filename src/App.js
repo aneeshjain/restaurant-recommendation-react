d1792cf8e8d00a2fcf243f594d24435ca1029071
aneeshjain <aneeshjain70@gmail.com>
2023-06-01 19:35:38 -0700

    Added top nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Business from './Business.js';
+import BusinessList from './BusinessList.js';
+import SearchBar from './SearchBar.js';
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='nav-bar'>ravenous</nav>
+      <SearchBar />
     </div>
   );
 }
